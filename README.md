e377683f10dc802ff0d1f5c59c59a648dd181353
Skachkov Maxim <AngelSamAdd@gmail.com>
2025-03-31 16:19:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# node-mongo
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Start Server
+
+```sh
+npm --env-file=.env index.js
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+const mongoClient = new MongoClient(process.env.URL_CONNECTION);
+
+(async () => {
+  try {
+    await mongoClient.connect();
+    app.locals.db = mongoClient.db("dndmapinfo");
+    app.listen(3000);
+    console.log("Server start.");
+  } catch (err) {
+    console.log(err);
+  }
+})();
+
+app.get("/", async (req, res) => {
+  res.json("Welcom to node-mongo server.");
+});
+
+app.get("/api/character", async (req, res) => {
+  console.log("Server GET /api/character/");
+  const collection = req.app.locals.db.collection("sailpunkCharacter");
+
+  try {
+    const characters = await collection.find({}).toArray();
+    console.log("Status code: 200");
+    res.send(characters);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/api/mapObject", async (req, res) => {
+  console.log("Server GET /api/mapObject/");
+  const collection = req.app.locals.db.collection("sailpunkMapObject");
+
+  try {
+    const mapObject = await collection.find({}).toArray();
+    console.log("Status code: 200");
+    res.send(mapObject);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+process.on("SIGINT", async () => {
+  await mongoClient.close();
+  console.log("Server stop.");
+  process.exit();
+});
